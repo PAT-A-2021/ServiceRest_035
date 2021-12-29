2bc55ad9691df79783cf43605458708c22e28a9f
sekarputrii <sekarputriretno83@gmail.com>
2021-12-29 12:18:09 +0700

    Add project files.

---
+++ ServiceRest_20190140035_SekarPutriRetno/ITI_UMY.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace ServiceRest_20190140035_SekarPutriRetno
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface ITI_UMY
+    {
+        [OperationContract]
+        [WebGet(UriTemplate = "Mahasiswa", ResponseFormat = WebMessageFormat.Json)] //untuk membuat slash, selalu relative
+        List<Mahasiswa> GetAllMahasiswa();  //mendapatkan kumpulan mahasiswa/seluruh data mahasiswa
+        [OperationContract]
+        [WebGet(UriTemplate = "Mahasiswa/nim={nim}", ResponseFormat = WebMessageFormat.Json)]   //untuk get
+        Mahasiswa GetMahasiswaByNIM(string nim);    //mengambil data berdasarkan nim
+
+        //void CreateMahasiswa(Mahasiswa mhs); tidak ada pengembalian/respond balik
+        [OperationContract]
+        [WebInvoke(Method = "POST", UriTemplate = "Mahasiswa", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
+        string CreateMahasiswa(Mahasiswa mhs);
+
+        //TODO: Add your service operations here
+    }
+
+    [DataContract]
+    public class Mahasiswa
+    {
+        private string _nama, _nim, _prodi, _angkatan;  // _ adalah konvensi atau kesepakatan   //variabel lokal
+        [DataMember(Order = 1)] //mengirim data untuk mengurutkan
+        public string nama
+        {
+            get { return _nama; }
+            set { _nama = value; }
+        }
+        [DataMember(Order = 2)]
+        public string nim
+        {
+            get { return _nim; }
+            set { _nim = value; }
+        }
+        [DataMember(Order = 3)]
+        public string prodi
+        {
+            get { return _prodi; }
+            set { _prodi = value; }
+        }
+        [DataMember(Order = 4)]
+        public string angkatan
+        {
+            get { return _angkatan; }
+            set { _angkatan = value; }
+        }
+    }
+}
